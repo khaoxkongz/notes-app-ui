389cd6bc615d4e4d2f9859be89a75c0082883b97
khaoxkongz <thanapat.makkawal@gmail.com>
2024-02-20 21:25:41 +0700

    feat: adding ui elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const App = () => {
+	return (
+		<div className="app-container">
+			<form action="" className="note-form">
+				<input placeholder="title" required />
+				<textarea placeholder="Content" rows={10} required />
+				<button type="submit">Add Note</button>
+			</form>
+			<div className="notes-grid">
+				<div className="note-item">
+					<div className="notes-header">
+						<button>x</button>
+					</div>
+					<h2>Note Title</h2>
+					<p>Note Content</p>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+export default App;
