1a936a36763a8fcb3ede01eff5a0feced583719d
khaoxkongz <thanapat.makkawal@gmail.com>
2024-02-20 21:48:30 +0700

    feat: displaying notes on the ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,36 @@
-import "./App.css"
+import { useState } from "react";
+import "./App.css";
+
+type Note = {
+	id: number;
+	title: string;
+	content: string;
+};
 
 const App = () => {
+	const [notes, setNotes] = useState<Note[]>([
+		{
+			id: 1,
+			title: "note title 1",
+			content: "content 1",
+		},
+		{
+			id: 2,
+			title: "note title 2",
+			content: "content 2",
+		},
+		{
+			id: 3,
+			title: "note title 3",
+			content: "content 3",
+		},
+		{
+			id: 4,
+			title: "note title 4",
+			content: "content 4",
+		},
+	]);
+
 	return (
 		<div className="app-container">
 			<form action="" className="note-form">
@@ -9,13 +39,15 @@ const App = () => {
 				<button type="submit">Add Note</button>
 			</form>
 			<div className="notes-grid">
-				<div className="note-item">
-					<div className="notes-header">
-						<button>x</button>
+				{notes.map((note) => (
+					<div className="note-item">
+						<div className="notes-header">
+							<button>x</button>
+						</div>
+						<h2>{note.title}</h2>
+						<p>{note.content}</p>
 					</div>
-					<h2>Note Title</h2>
-					<p>Note Content</p>
-				</div>
+				))}
 			</div>
 		</div>
 	);
